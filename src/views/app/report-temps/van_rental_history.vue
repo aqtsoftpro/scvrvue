7ae241195c53d48731036ac8307e49f300432c55
Afzaal565 <afzaalhussain565@gmail.com>
2024-03-27 16:30:19 +0500

    rental history page updated

--- src/views/app/report-temps/van_rental_history.vue
+++ src/views/app/report-temps/van_rental_history.vue
@@ -2,8 +2,24 @@
   <div>
     <b-row>
       <b-colxx xxs="12">
-          <b-card class="mb-4"  >
-            <datatable :title="$t('menu.reports.van-rental-history')" :searchColumn="searchColumns" :data="rental_history" :fields="fields" />
+          <b-card class="mb-4" :title="$t('menu.reports.van-rental-history')" >
+            <b-row>
+              <b-colxx>
+                <b-form inline class="float-end">
+                  <b-form-input style="display:none" type="text" v-model.trim="form.start_date" />
+                  <datepicker :default-value="today" type="datetime" placeholder="start date" value-type="format"
+                    v-model="form.start_date" format="DD-MM-YYYY"></datepicker>
+
+                  <b-form-input style="display:none" type="text" v-model.trim="form.end_date" />
+                  <datepicker :default-value="today" type="datetime" placeholder="start date" value-type="format"
+                    v-model="form.end_date" format="DD-MM-YYYY"></datepicker>
+
+                  <b-button variant="primary" class="ms-1" @click="filterByDate">Filter</b-button>
+                </b-form>
+              </b-colxx>
+            </b-row>
+            <!-- :title="$t('menu.reports.van-rental-history')" -->
+            <datatable :searchColumn="searchColumns" :data="rental_history" :fields="fields" />
           </b-card>
           <button @click="()=>{$router.push('/app/reports')}" class="btn btn-primary">Back to reports menu</button>
           <button  class="btn btn-outline-success" @click.prevent="exportToExcel">Export to Excel</button>
@@ -13,12 +29,14 @@
 </template>
 <script>
 import datatable from './datatable'
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
 import { apiUrl } from '../../../constants/config'
 import axios from 'axios'
 import { utils, writeFileXLSX } from 'xlsx';
 export default ({
   components: {
-    'datatable': datatable
+    'datatable': datatable, datepicker: DatePicker
   },
   data() {
     return {
@@ -57,10 +75,27 @@ export default ({
           dataClass: "muted-text",
           width: "20%"
         },
-      ]
+      ],
+      form: {
+        start_date: '',
+        end_date: '',
+      },
     }
   },
   methods: {
+    filterByDate() {
+      axios.get(apiUrl + '/reports/rental_history', {
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.getItem('token')
+        },
+        params: {
+          start_date: this.form.start_date, // Corrected reference
+          end_date: this.form.end_date
+        }
+      }).then(response => {
+        this.rental_history = response.data
+      })
+    },
 
     exportToExcel(){
         /* generate worksheet from state */
