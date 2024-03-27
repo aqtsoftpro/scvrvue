85f8c7998962659514d06ef560f40c302331c90d
Afzaal565 <afzaalhussain565@gmail.com>
2024-03-27 16:03:27 +0500

    maintenence cost and view earning fixed

--- src/views/app/report-temps/view_earnings.vue
+++ src/views/app/report-temps/view_earnings.vue
@@ -2,36 +2,27 @@
   <div>
     <b-row>
       <b-colxx xxs="12">
-        <b-card class="mb-4">
-          <b-form>
-            <b-row class="d-flex justify-conten-center">
-              <b-colxx lg="4" class="mb-3 float-right">
-                <b-form class="av-tooltip tooltip-label-right">
-                  <div>Start date</div>
-                  <b-form-input style="display:none" type="text"
-                    />
-                  <datepicker :default-value="today" type="datetime"
-                    placeholder="start date"
-                    value-type="format" format="DD-MM-YYYY h:mm"></datepicker>
-                  <b-form-invalid-feedback > Rental </b-form-invalid-feedback>
-                </b-form>
-              </b-colxx>
-              <b-colxx lg="4" class="mb-3">
-                <b-form class="av-tooltip tooltip-label-right">
-                  <div>Start date</div>
-                  <b-form-input style="display:none" type="text"
-                    />
-                  <datepicker :default-value="today" type="datetime"
-                    placeholder="start date"
-                    value-type="format" format="DD-MM-YYYY h:mm"></datepicker>
-                  <b-form-invalid-feedback > Rental </b-form-invalid-feedback>
-                </b-form>
-              </b-colxx>
-            </b-row>
-          </b-form>
+        <b-card class="mb-4" :title="$t('menu.reports.view-earnings')">
+          <b-row>
+            <b-colxx> 
+              <b-form inline class="float-end">
+                <b-form-input style="display:none" type="text" v-model.trim="form.start_date" />
+                <datepicker :default-value="today" type="datetime" placeholder="start date" value-type="format"
+                  v-model="form.start_date"
+                  format="DD-MM-YYYY"></datepicker>
+
+                <b-form-input style="display:none" type="text" v-model.trim="form.end_date" />
+                <datepicker :default-value="today" type="datetime" placeholder="start date" value-type="format"
+                  v-model="form.end_date"
+                  format="DD-MM-YYYY"></datepicker>
+
+                <b-button variant="primary" class="ms-1" @click="filterByDate">Filter</b-button>
+              </b-form>
+            </b-colxx>
+          </b-row>
+
           <!-- :title="$t('menu.reports.view-earnings')" -->
-          <datatable :sum="sum" :searchColumn="searchColumns" :data="earnings"
-            :fields="fields" />
+          <datatable :sum="sum" :searchColumn="searchColumns" :data="earnings" :fields="fields" />
         </b-card>
         <button @click="() => { $router.push('/app/reports') }" class="btn btn-primary">Back to reports menu</button>
         <button class="btn btn-outline-success" @click.prevent="exportToExcel">Export to Excel</button>
@@ -53,6 +44,7 @@ export default ({
   },
   data() {
     return {
+      today: new Date(),
       earnings: [],
       sum: 0,
       searchColumns: ['vehicle', 'date', 'amount'],
@@ -81,11 +73,30 @@ export default ({
           dataClass: "muted-text",
           width: "20%"
         },
-      ]
+      ],
+
+      form: {
+        start_date: '',
+        end_date: '',
+      },
     }
   },
 
   methods: {
+    filterByDate() {
+      axios.get(apiUrl + '/reports/earnings', {
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.getItem('token')
+        },
+        params: {
+          start_date: this.form.start_date, // Corrected reference
+          end_date: this.form.end_date
+        }
+      }).then(response => {
+        this.earnings = response.data.earnings
+        this.sum = response.data.total
+      })
+    },
     exportToExcel() {
       /* generate worksheet from state */
       const ws = utils.json_to_sheet(this.earnings);
--- src/views/app/report-temps/view_maintenance_cost.vue
+++ src/views/app/report-temps/view_maintenance_cost.vue
@@ -2,13 +2,29 @@
   <div>
     <b-row>
       <b-colxx xxs="12">
-          <b-card class="mb-4"  >
-            <datatable :sum="sum" :title="$t('menu.reports.view-maintenance_cost')" :searchColumn="searchColumns" :data="maintenance_cost" :fields="fields" />
-            <!-- <table><tr><td><h1>Total:</h1></td><td><h1 class="text-primary">{{total}}</h1></td></tr></table> -->
-          </b-card>
+        <b-card class="mb-4" :title="$t('menu.reports.view-maintenance_cost')">
+          <b-row>
+            <b-colxx>
+              <b-form inline class="float-end">
+                <b-form-input style="display:none" type="text" v-model.trim="form.start_date" />
+                <datepicker :default-value="today" type="datetime" placeholder="start date" value-type="format"
+                  v-model="form.start_date" format="DD-MM-YYYY"></datepicker>
 
-          <button @click="()=>{$router.push('/app/reports')}" class="btn btn-primary">Back to reports menu</button>
-          <button  class="btn btn-outline-success" @click.prevent="exportToExcel">Export to Excel</button>
+                <b-form-input style="display:none" type="text" v-model.trim="form.end_date" />
+                <datepicker :default-value="today" type="datetime" placeholder="start date" value-type="format"
+                  v-model="form.end_date" format="DD-MM-YYYY"></datepicker>
+
+                <b-button variant="primary" class="ms-1" @click="filterByDate">Filter</b-button>
+              </b-form>
+            </b-colxx>
+          </b-row>
+          <!-- :title="$t('menu.reports.view-maintenance_cost')" -->
+          <datatable :sum="sum" :searchColumn="searchColumns" :data="maintenance_cost" :fields="fields" />
+          <!-- <table><tr><td><h1>Total:</h1></td><td><h1 class="text-primary">{{total}}</h1></td></tr></table> -->
+        </b-card>
+
+        <button @click="() => { $router.push('/app/reports') }" class="btn btn-primary">Back to reports menu</button>
+        <button class="btn btn-outline-success" @click.prevent="exportToExcel">Export to Excel</button>
       </b-colxx>
     </b-row>
   </div>
@@ -16,16 +32,18 @@
 <script>
 import axios from 'axios';
 import { apiUrl } from '../../../constants/config';
-import datatable from './datatable'
+import datatable from './datatable';
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
 import { utils, writeFileXLSX } from 'xlsx';
 export default ({
   components: {
-    'datatable': datatable
+    'datatable': datatable, datepicker: DatePicker
   },
   data() {
     return {
       maintenance_cost: [],
-      searchColumns : ['date', 'vehicle', 'service', 'cost', 'sum'],
+      searchColumns: ['date', 'vehicle', 'service', 'cost', 'sum'],
       fields: [
         {
           name: "date",
@@ -59,39 +77,56 @@ export default ({
           dataClass: "muted-text",
           width: "20%"
         },
-      ]
+      ],
+      form: {
+        start_date: '',
+        end_date: '',
+      },
     }
   },
 
   methods: {
-    exportToExcel(){
-        /* generate worksheet from state */
-        const ws = utils.json_to_sheet(this.maintenance_cost);
-        /* create workbook and append worksheet */
-        const wb = utils.book_new();
-        utils.book_append_sheet(wb, ws, "Data");
-        /* export to XLSX */
-        writeFileXLSX(wb, "scvr_maintenance_cost.xlsx");
-    },
-    get_maintenance_entries(){
-      axios.get(apiUrl + '/reports/maintenance_cost', {headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')}})
-      .then(response => {
-        this.maintenance_cost = response.data.maintenance
-        this.sum = response.data.total
+    filterByDate() {
+      axios.get(apiUrl + '/reports/maintenance_cost', {
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.getItem('token')
+        },
+        params: {
+          start_date: this.form.start_date, // Corrected reference
+          end_date: this.form.end_date
+        }
+      }).then(response => {
+          this.maintenance_cost = response.data.maintenance
+          this.sum = response.data.total
       })
+    },
+
+    exportToExcel() {
+      /* generate worksheet from state */
+      const ws = utils.json_to_sheet(this.maintenance_cost);
+      /* create workbook and append worksheet */
+      const wb = utils.book_new();
+      utils.book_append_sheet(wb, ws, "Data");
+      /* export to XLSX */
+      writeFileXLSX(wb, "scvr_maintenance_cost.xlsx");
+    },
+    get_maintenance_entries() {
+      axios.get(apiUrl + '/reports/maintenance_cost', { headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') } })
+        .then(response => {
+          this.maintenance_cost = response.data.maintenance
+          this.sum = response.data.total
+        })
     }
   },
-  mounted(){
+  mounted() {
 
     this.get_maintenance_entries();
 
-    if(localStorage.getItem('token') !== null) {
+    if (localStorage.getItem('token') !== null) {
       this.$store.commit('setUser', JSON.parse(localStorage.getItem('user')));
-    } else{
+    } else {
       this.$router.push('/user/login');
     }
   }
 })
 </script>
-
-
