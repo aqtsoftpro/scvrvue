b1f5a42f635da85d6a68635506e8ccdfed75ddb6
Afzaal565 <afzaalhussain565@gmail.com>
2024-03-30 13:58:49 +0500

    loss profit page updated

--- src/views/app/report-temps/datatable.vue
+++ src/views/app/report-temps/datatable.vue
@@ -36,6 +36,9 @@
           <template slot="cost" slot-scope="props">
             ${{props.rowData.cost}}
           </template>
+          <template slot="expense" slot-scope="props">
+            ${{props.rowData.expense}}
+          </template>
           <template slot="sub_total" slot-scope="props">
             ${{props.rowData.sub_total}}
           </template>
--- src/views/app/report-temps/profit_and_loss.vue
+++ src/views/app/report-temps/profit_and_loss.vue
@@ -3,6 +3,25 @@
     <b-row>
       <b-colxx xxs="12">
           <b-card class="mb-4"  >
+            <b-row>
+              <b-colxx>
+                <b-form inline class="float-end" style="justify-content: flex-end !important;">
+                  <div style="margin-right: 1em !important;">
+                    <b-form-input style="display:none" type="text" v-model.trim="form.start_date" />
+                    <datepicker :default-value="today" type="date" placeholder="start date" value-type="format"
+                      v-model="form.start_date" format="DD-MM-YYYY"></datepicker>
+                  </div>
+
+                  <div style="margin-right: 1em !important;">
+                    <b-form-input style="display:none" type="text" v-model.trim="form.end_date" />
+                    <datepicker :default-value="today" type="date" placeholder="start date" value-type="format"
+                      v-model="form.end_date" format="DD-MM-YYYY"></datepicker>
+                  </div>
+
+                  <b-button variant="primary" class="ms-1" @click="filterByDate">Filter</b-button>
+                </b-form>
+              </b-colxx>
+            </b-row>
             <datatable :title="$t('menu.reports.profit-and-loss')" :searchColumn="searchColumns" :data="pnl_records" :fields="fields" />
           </b-card>
           <button @click="()=>{$router.push('/app/reports')}" class="btn btn-primary">Back to reports menu</button>
@@ -15,16 +34,19 @@
 import datatable from './datatable'
 import axios from 'axios'
 import { apiUrl } from '../../../constants/config'
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
 import { utils, writeFileXLSX } from 'xlsx';
 
 export default ({
   components: {
-    'datatable': datatable
+    'datatable': datatable, datepicker: DatePicker
   },
   data() {
     return {
+      today: new Date(),
       pnl_records: [],
-      searchColumns: ['date', 'notes', 'cost', 'sub_total'],
+      searchColumns: ['date', 'notes', 'cost', 'expense', 'sub_total'],
       fields: [
         {
           name: "date",
@@ -51,6 +73,14 @@ export default ({
           width: "20%"
         },
         {
+          name: "__slot:expense",
+          sortField: "expense",
+          title: "Expenses",
+          titleClass: "",
+          dataClass: "text-muted",
+          width: "20%"
+        },
+        {
           name: "__slot:sub_total",
           sortField: "sub_toal",
           title: "Sub Total",
@@ -58,7 +88,11 @@ export default ({
           dataClass: "text-muted",
           width: "20%"
         },
-      ]
+      ],
+      form: {
+        start_date: '',
+        end_date: '',
+      },
     }
   },
   methods: {
@@ -78,7 +112,21 @@ export default ({
       .then(response => {
         this.pnl_records = response.data
       })
-    }
+    },
+
+    filterByDate() {
+      axios.get(apiUrl + '/reports/profit_loss', {
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.getItem('token')
+        },
+        params: {
+          start_date: this.form.start_date, // Corrected reference
+          end_date: this.form.end_date
+        }
+      }).then(response => {
+        this.pnl_records = response.data
+      })
+    },
   },
   mounted(){
 
