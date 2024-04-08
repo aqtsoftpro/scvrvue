d58b2a1d39fef954b7dfa28d98bb5d1f55a06597
Afzaal565 <afzaalhussain565@gmail.com>
2024-04-08 15:55:02 +0500

    new update for import excel in toll

--- src/constants/config.js
+++ src/constants/config.js
@@ -27,6 +27,7 @@ export const firebaseConfig = {
 //Local api
 // export const apiUrl = 'http://127.0.0.1:8000/api'
 //remote api
+
 export const apiUrl = 'http://scvr-test.aqtdemos.com/public/api'
 export const currentUser = {
   id: 1,
