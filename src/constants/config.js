9692ca7c30f88a2a093a542e6b827f012852a48e
Afzaal565 <afzaalhussain565@gmail.com>
2024-04-16 12:14:36 +0500

    new updates for build fix

--- src/constants/config.js
+++ src/constants/config.js
@@ -28,7 +28,6 @@ export const firebaseConfig = {
 export const apiUrl = 'http://127.0.0.1:8000/api'
 
 //remote api
-
 // export const apiUrl = 'http://scvr-test.aqtdemos.com/public/api'
 export const currentUser = {
   id: 1,
